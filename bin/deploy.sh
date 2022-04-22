996365fe70c516c8c819b8ce3698b2f6dd5645a1
MEDL <72726208+MEDUELLINKS@users.noreply.github.com>
2022-04-23 01:17:29 +0200

    new server

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read ./udagram-frontend/www s3://udagram-frontend1345234/
+aws s3 cp --recursive --acl public-read ./udagram-frontend/www s3://udagram-frontend48456745/
