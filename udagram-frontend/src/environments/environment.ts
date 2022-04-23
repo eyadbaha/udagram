a5fd8fd3742c3de603beb41c6ce5e670c7753162
MEDL <72726208+MEDUELLINKS@users.noreply.github.com>
2022-04-24 01:44:15 +0200

    new server

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagramapi-env.eba-avg3gfi2.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://Udagramapi-env.eba-mgmxxgd9.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
